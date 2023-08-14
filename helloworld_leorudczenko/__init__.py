63976c04cfbf5ecb699f2ce69ac6f13067945b55
Leo Rudczenko <leorud@bgs.ac.uk>
2023-08-14 16:54:04 +0100

    Add __version__

--- helloworld_leorudczenko/__init__.py
+++ helloworld_leorudczenko/__init__.py
@@ -1,3 +1,14 @@
+from pkg_resources import (
+    get_distribution,
+    DistributionNotFound,
+)
+
 from helloworld_leorudczenko.myscript import spacing
 from helloworld_leorudczenko import extra
 from helloworld_leorudczenko import testing
+
+
+try:
+    __version__ = get_distribution(__name__)
+except DistributionNotFound:
+    pass
